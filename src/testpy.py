2d7b7412e43301ecbc252377422f629f67c335a7
pythonms <pythonms@gmail.com>
2012-10-03 21:27:04 +0200

    po odzyskanoiu pliku

---
+++ src/testpy.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+def foo():
+    print 'foo'
+    
+    
+x = 123
+y = 'abc'
+
+print x,y
+print 'end'
+foo()
+#dodalem komentarz ąśżźńółÓŁąśę
+print os.name
+print os.__doc__
+print os.environ 
+print 'ąśę'
