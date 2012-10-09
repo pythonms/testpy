5424d1c70547d2ef1d0bfe639c0a978f22f532bd
mszot <mszot@CENTRUM-NASA>
2012-10-09 09:46:57 +0200

    zmiany

--- src/subproc.py
+++ src/subproc.py
@@ -8,4 +8,19 @@ Created on 09-10-2012
 
 import subprocess
 
-print subprocess.call(["ls", "-l"], shell = False)
+print subprocess.call(["ls", "-l","-a","-s"], shell = True)
+print subprocess.call("exit 1", shell = True)
+#Run command with arguments. Wait for command to complete. and 
+#If the return code was zero then return, otherwise raise CalledProcessError. 
+print subprocess.check_call(["ls", "-l"])
+print subprocess.check_call("exit 0", shell=True)
+print subprocess.check_output(["echo", "Hello World!"], shell=True)
+print subprocess.check_output("dir",stderr=subprocess.STDOUT,shell=True)
+
+import shlex
+print 'podaj komende:',
+command_line = 'ls'
+#command_line = raw_input()
+args = shlex.split(command_line)
+print args
+p = subprocess.Popen(args)
