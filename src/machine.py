9004bd2efb7614abfbe08c124439d8b0cb13253f
mszot <mszot@CENTRUM-NASA>
2012-10-10 10:06:21 +0200

    doc function add
    Signed-off-by: mszot <mszot@CENTRUM-NASA>

--- src/machine.py
+++ src/machine.py
@@ -10,8 +10,10 @@ import platform
 
 
 def get_machine_info():
+    """Get machine info system_ver_32/64bit_hostname_pyVer"""
     machine_id = platform.system() +"_"+ platform.release() +'_' \
-            + platform.machine() +'_'+platform.node()+ '_pyVer:' + platform.python_version()
+            + platform.machine() +'_'+platform.node()+ '_pyVer:' \
+            + platform.python_version()
     return machine_id
 
 
